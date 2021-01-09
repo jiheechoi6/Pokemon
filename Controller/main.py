aeec1c169c2a0c01d0a3f4ccda603bf221891f77
Emma Paris <christine.choi@mail.utoronto.ca>
2021-01-10 03:34:09 +0900

    choose a pokemon

--- Controller/main.py
+++ Controller/main.py
@@ -7,27 +7,37 @@ from Presenter.graphics import Graphics
 graphics = Graphics()
 graphics.draw_start_game_screen()
 
-start_game_screen = True
+start_game_scene = True
+choose_pokemon_scene = False
 grass_scene = False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if start_game_screen:
+        if start_game_scene:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if 190 < event.pos[0] < 690 and 270<event.pos[1]<379:
-                    graphics.draw_grass_scene()
-                    grass_scene = True
-                    start_game_screen = False
-        if grass_scene:
+                    graphics.draw_choose_a_pokemon()
+                    choose_pokemon_scene = True
+                    start_game_scene = False
+        elif choose_pokemon_scene:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 80 < event.pos[0] < 230 and 270 < event.pos[1] < 420:
+                    graphics.draw_grass_scene(0)
+                if 370 < event.pos[0] < 520 and 270 < event.pos[1] < 420:
+                    graphics.draw_grass_scene(1)
+                if 640 < event.pos[0] < 790 and 270 < event.pos[1] < 420:
+                    graphics.draw_grass_scene(2)
+                grass_scene = True
+                choose_pokemon_scene = False
+        elif grass_scene:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     graphics.draw_moving_player(True, False, False, False)
                 if event.key == pygame.K_DOWN:
                     graphics.draw_moving_player(False, False, True, False)
                 if event.key == pygame.K_LEFT:
-                    print("left")
                     graphics.draw_moving_player(False, False, False, True)
                 if event.key == pygame.K_RIGHT:
                     graphics.draw_moving_player(False, True, False, False)
--- Presenter/graphics.py
+++ Presenter/graphics.py
@@ -1,4 +1,5 @@
 import random
+import string
 from typing import List, Tuple
 
 import pygame
@@ -8,6 +9,7 @@ class Graphics:
     # grass scene attributes
     _door_positions: List[Tuple[int, int]]
     _player_pos: List[int]
+    _player_img_small: string
 
     def __init__(self):
         self._door_positions = []
@@ -16,7 +18,9 @@ class Graphics:
 
     def draw_start_game_screen(self):
         screen = pygame.display.get_surface()
-        pygame.draw.rect(screen, (255, 128, 128), (0, 0, 900, 700))
+        background_img = pygame.image.load('./img/background.png')
+        screen.blit(background_img, (0, 0))
+        pygame.draw.rect(screen, (255, 228, 228), (167, 266, 540, 120))
         image = pygame.image.load('./img/start_game_button.png')
         screen.blit(image, (190, 270))
 
@@ -32,7 +36,11 @@ class Graphics:
                 if j == draw or j == draw2:
                     self._door_positions.append((x_pos, y_pos))
 
-    def draw_grass_scene(self):
+    '''@param pokemon_type: the type of pokemon the player chose.
+                            0 - the leftmost pokemon
+                            1 - middle pokemon
+                            2 - rightmost pokemon'''
+    def draw_grass_scene(self, pokemon_type: int):
         screen = pygame.display.get_surface()
         pygame.draw.rect(screen, (193, 204, 80), (0, 0, 900, 700)) # set background to green
         image = pygame.image.load('./img/door.png')
@@ -49,7 +57,14 @@ class Graphics:
                 screen.blit(image2, (x_pos, y_pos))
 
         # draw player
-        player_img = pygame.image.load('./img/egg.png')
+        if pokemon_type == 0:
+            self._player_img_small = './img/pokemon_0_small.png'
+        elif pokemon_type == 1:
+            self._player_img_small = './img/pokemon_1_small.png'
+        else:
+            self._player_img_small = './img/pokemon_2_small.png'
+
+        player_img = pygame.image.load(self._player_img_small)
         screen.blit(player_img, (420, 300))
         self._player_pos = [420, 300]
 
@@ -57,7 +72,7 @@ class Graphics:
 
     def draw_moving_player(self, up:bool, right:bool, down: bool, left: bool):
         screen = pygame.display.get_surface()
-        player_img = pygame.image.load('./img/egg.png')
+        player_img = pygame.image.load(self._player_img_small)
         image = pygame.image.load('./img/door.png')
         image2 = pygame.image.load('./img/grass2.png')
 
@@ -83,4 +98,21 @@ class Graphics:
         screen.blit(player_img, (self._player_pos[0], self._player_pos[1]))
         pygame.display.update()
 
+    def draw_choose_a_pokemon(self):
+        print("check")
+        screen = pygame.display.get_surface()
+        background_img = pygame.image.load('./img/background_light.png')
+        pokemon0_img = pygame.image.load('./img/pokemon_0.png')
+        pokemon1_img = pygame.image.load('./img/pokemon_1.png')
+        pokemon2_img = pygame.image.load('./img/pokemon_2.png')
 
+        my_font = pygame.font.SysFont('freesansbold', 55)
+        label = my_font.render("Choose a pokemon!", True, (0, 0, 0))
+
+        screen.blit(background_img, (0, 0))
+        screen.blit(label, (250, 150))  # instruction ("choose a pokemon")
+        screen.blit(pokemon0_img, (80, 272))  # first pokemon
+        screen.blit(pokemon1_img, (370, 280))  # second pokemon
+        screen.blit(pokemon2_img, (640, 260))  # third pokemon
+
+        pygame.display.update()
