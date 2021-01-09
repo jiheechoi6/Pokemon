22185c1259a51770b45349b33fe4e57e3ea4065f
Emma Paris <christine.choi@mail.utoronto.ca>
2021-01-09 22:45:12 +0900

    start game screen done

--- Presenter/graphics.py
+++ Presenter/graphics.py
@@ -8,8 +8,8 @@ class Graphics:
 
     def draw_start_game_screen(self):
         screen = pygame.display.get_surface()
+        pygame.draw.rect(screen, (255, 128, 128), (0, 0, 900, 700))
         image = pygame.image.load('start_game_button.png')
-        pygame.transform.scale(image, (1, 1))
         screen.blit(image, (190, 270))
 
         pygame.display.update()
